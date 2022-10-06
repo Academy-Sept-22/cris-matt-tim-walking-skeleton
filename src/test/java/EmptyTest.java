bc30f2379207bed3eb345d9be95d8faaf9d22c98
Tim Turvey <tturvey@hotmail.co.uk>
2022-10-06 15:43:26 +0100

    Empty test

---
+++ src/test/java/EmptyTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.*;
+
+public class EmptyTest {
+
+    @Test
+    void
+    empty_test() {
+        assertEquals(1, 1);
+    }
+
+}
